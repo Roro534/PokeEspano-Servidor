1ae0b7070b4ddf1a4118d2eb0e09de63e7a21897
AllCarb152 <Plexus_12@hotmail.com>
2015-09-05 15:28:36 -0430

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -15,7 +15,7 @@ exports.proxyip = ['127.0.0.0/8'];
 //   in every Random Battle team.
 exports.potd = '';
 exports.superAdmins = {
-	"brekfree": 1
+	"allcarb152": 1
 };
 
 // crash guard - write errors to log file instead of crashing
